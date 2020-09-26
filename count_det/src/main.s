b36abf91d26a2bed3e96ce0917af9d8dc5fe9c0d
Alexandr Sokolov <aesokolov1975@gmail.com>
2020-09-26 14:07:34 +0300

    Some optimization of main.s

--- count_det/src/main.s
+++ count_det/src/main.s
@@ -18,35 +18,23 @@ count_det:
 	mov r3, r7
 	mov r6, r7
 	ldr r7, [r0], #4	//a2
-	mov r8, r3		
-	mul r3, r8, r7		//a2*c1
-	mov r8, r5
-	mul r5, r8, r7		// -a2* b1//
+	mul r3, r3, r7		//a2*c1
+	mul r5, r5, r7		// -a2* b1//
 	ldr r7, [r0], #4	//b2
-	mov r8, r1
-	mul r1, r8, r7		//a1 * b2
-	mov r8, r6
-	mul r6, r8, r7		//- b2 * c1  
+	mul r1, r1, r7		//a1 * b2
+	mul r6, r6, r7		//- b2 * c1  
 	ldr r7, [r0], #4	//c2
-	mov r8, r2
-	mul r2, r8, r7		//b1 * c2
-	mov r8, r4
-	mul r4, r8, r7		//- a1 * c2
+	mul r2, r2, r7		//b1 * c2
+	mul r4, r4, r7		//- a1 * c2
 	ldr r7, [r0], #4	//a3
-	mov r8, r2
-	mul r2, r8, r7		//a3 * b1 * c2
-	mov r8, r6
-	mul r6, r8, r7		//-a3 * b2 * c1
+	mul r2, r2, r7		//a3 * b1 * c2
+	mul r6, r6, r7		//-a3 * b2 * c1
 	ldr r7, [r0], #4	//b3
-	mov r8, r3
-	mul r3,r8, r7		//a2 * c2 * b3
-	mov r8, r4
-	mul r4, r8, r7		//-a1 *c3 * b3
+	mul r3,r3, r7		//a2 * c2 * b3
+	mul r4, r4, r7		//-a1 *c3 * b3
 	ldr r7, [r0], #4	//c3
-	mov r8, r1
-	mul r1, r8, r7		//a1 * b2 * c3
-	mov r8, r5
-	mul r5, r8, r7		//-a2 * b1 * c3
+	mul r1, r1, r7		//a1 * b2 * c3
+	mul r5, r5, r7		//-a2 * b1 * c3
 	add r0, r1 ,r2		
 	add r0, r3
 	sub r0, r4
